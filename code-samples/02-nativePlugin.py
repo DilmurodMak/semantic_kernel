d7b6a2312f20d4da040379e2c09694443980e7d8
Dilik <1989deny@gmail.com>
2025-08-05 18:32:59 +0500

    formatiing

--- code-samples/02-nativePlugin.py
+++ code-samples/02-nativePlugin.py
@@ -13,17 +13,19 @@ load_dotenv()
 
 service_id = "default"
 kernel.add_service(
-    AzureChatCompletion(service_id=service_id,
-                        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-                        deployment_name=os.getenv("AZURE_OPENAI_CHAT_COMPLETION_MODEL"),
-                        endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    AzureChatCompletion(
+        service_id=service_id,
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        deployment_name=os.getenv("AZURE_OPENAI_CHAT_COMPLETION_MODEL"),
+        endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     )
 )
 
 
 class Math:
     """
-    Description: MathPlugin provides a set of functions to make Math calculations.
+    Description: MathPlugin provides a set of
+                    functions to make Math calculations.
 
     Usage:
         kernel.add_plugin(MathPlugin(), plugin_name="math")
@@ -91,7 +93,9 @@ math_plugin = kernel.add_plugin(Math(), "Math")
 
 sqrt_function = math_plugin["Sqrt"]
 
+
 async def square_root():
-    return await kernel.invoke(sqrt_function, number1=4)
+    return await kernel.invoke(sqrt_function, number1=154)
+
 
 print(asyncio.run(square_root()))
--- code-samples/03-planner.py
+++ code-samples/03-planner.py
@@ -4,41 +4,44 @@ import asyncio
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from dotenv import load_dotenv
 from semantic_kernel.planners import SequentialPlanner
+
 kernel = Kernel()
 
 service_id = "default"
 
 load_dotenv()
 kernel.add_service(
-    AzureChatCompletion(service_id=service_id,
-                        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-                        deployment_name=os.getenv("AZURE_OPENAI_CHAT_COMPLETION_MODEL"),
-                        endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    AzureChatCompletion(
+        service_id=service_id,
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        deployment_name=os.getenv("AZURE_OPENAI_CHAT_COMPLETION_MODEL"),
+        endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     )
 )
 
-planner = SequentialPlanner(
-    kernel,
-    service_id
-)
+planner = SequentialPlanner(kernel, service_id)
 
-kernel.add_plugin(parent_directory="../plugins/prompt_templates/", plugin_name="writerPlugin")
+kernel.add_plugin(
+    parent_directory="../plugins/prompt_templates/", plugin_name="writerPlugin"
+)
 
 for plugin_name, plugin in kernel.plugins.items():
     for function_name, function in plugin.functions.items():
         print(f"Plugin: {plugin_name}, Function: {function_name}")
-        
-text=" "
-        
+
+text = " "
+
 with open("../data/chatgpt.txt", "r") as file:
     chatgpt = file.read()
-    text = text+chatgpt
-    
-    
-goal = f"summarise this text: {text} and email it to sam@gmail.com "
-        
+    text = text + chatgpt
+
+
+goal = f"summarise this text: {text} and email it to mak@gmail.com "
+
+
 async def call_planner():
     return await planner.create_plan(goal)
+
 
 sequential_plan = asyncio.run(call_planner())
 
@@ -48,8 +51,10 @@ for step in sequential_plan._steps:
         f"- {step.description.replace('.', '') if step.description else 'No description'} using {step.metadata.fully_qualified_name} with parameters: {step.parameters}"
     )
 
+
 async def generate_answer():
     return await sequential_plan.invoke(kernel)
+
 
 result = asyncio.run(generate_answer())
 
